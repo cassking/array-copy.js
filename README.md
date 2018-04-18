5ad0c403f6844046b16abe24ac7db4fe06390136
Cassandra King <puddincat@gmail.com>
2018-04-17 20:51:39 -0400

    update js files

---
+++ README.md
@@ -0,0 +1,4 @@
+How to copy an Array ?
+-------
+
+Copying  Arrays Multiple ways in JavaScript |  Shallow Copy using Spread Operator and Object.assign.
---
+++ copy-array.js
@@ -0,0 +1,45 @@
+//1using map
+ let arr = [1,2,3]
+ let arr2 = arr.map(x => x)
+ console.log(arr2);
+
+//2 using json parse then stringify
+ arr2 = JSON.parse(JSON.stringify(arr))
+ console.log(arr2)
+
+//3 using slice
+let arr2 = arr.slice(0)
+ console.log(arr2)
+
+//4 for loop and push
+ let arr2= []
+ for(let i=0; i<arr.length;i++){
+   arr2.push(arr[i])
+ }
+ console.log(arr2)
+
+//5 object assign
+//Object.assign(target, ...sources)
+ let arr2 = Object.assign([], arr)
+  console.log(arr2);
+
+//6 spread operator
+  let arr2 = [...arr]
+  console.log(arr2);
+
+//7 concat
+ let arr = [].concat(arr2);
+  console.log(arr2);
+
+//9 array from
+//Array.from(arrayLike[, mapFn[, thisArg]
+//console.log(Array.from('foo'));
+// expected output: Array ["f", "o", "o"]
+//console.log(Array.from([1, 2, 3], x => x + x));
+// expected output: Array [2, 4, 6]
+ let arr2 = Array.from(arr)
+ console.log(arr2)﻿
+
+ //8 apply
+  let arr2 = arr.length === 1 ? [arr2[0]] : Array.apply(this, arr2);
+  console.log(arr)
